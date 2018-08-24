abd093d4c4fcf4d6b6efcfa6ccaee7955a58309c
Tornike jincharadze <tornikejincharadze1997@gmail.com>
2018-08-24 16:09:56 +0400

    changed styles

--- src/app/success-pay/success-pay.component.ts
+++ src/app/success-pay/success-pay.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success-pay',
   templateUrl: './success-pay.component.html',
-  styleUrls: ['./success-pay.component.scss']
+  styleUrls: ['./success-pay.component.scss','./success-pay-responsive.component.scss']
 })
 export class SuccessPayComponent implements OnInit {
 
