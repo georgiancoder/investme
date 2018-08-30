080ae71352922aadb021ded7c618ea6f60eb712e
Tornike jincharadze <tornikejincharadze1997@gmail.com>
2018-08-30 12:57:14 +0400

    styled suppoerters page

--- src/app/supporters/supporters.component.ts
+++ src/app/supporters/supporters.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-supporters',
   templateUrl: './supporters.component.html',
-  styleUrls: ['./supporters.component.scss']
+  styleUrls: ['./supporters.component.scss','./supporters.responsive.component.scss']
 })
 export class SupportersComponent implements OnInit {
-
+	flag=false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  activeMoreinfp(){
+  	this.flag=!this.flag;
+  }
 }
