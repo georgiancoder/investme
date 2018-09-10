0c01f0ce9dbc40b2b17b1e13f7487be8fb6dfc20
kaxo_22 <giokaxura@gmail.com>
2018-09-10 11:20:15 +0400

    bolo ganaxleba

--- src/app/add-project-page/add-project-page.component.ts
+++ src/app/add-project-page/add-project-page.component.ts
@@ -1,29 +1,31 @@
 import { Router } from '@angular/router';
 import { AuthService } from './../services/auth.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { TextPagesService } from '../services/text-pages.service';
 import { LangsService } from '../services/langs.service';
 import {AppComponent} from '../app.component';
+import { DomSanitizer} from '@angular/platform-browser';
 
 
 @Component({
   selector: 'app-add-project-page',
   templateUrl: './add-project-page.component.html',
-  styleUrls: ['./add-project-page.component.scss']
+  styleUrls: ['./add-project-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AddProjectPageComponent implements OnInit {
   breadcrumbs: object;
   lang:string;
   pageData:any;
   title: string;
-  description: string;
-  constructor(private langsservice: LangsService, private textPagesService: TextPagesService, private authService: AuthService, private router: Router, public myapp: AppComponent) { }
+  description: any;
+  constructor(private sanitizer:DomSanitizer,private langsservice: LangsService, private textPagesService: TextPagesService, private authService: AuthService, private router: Router, public myapp: AppComponent) { }
   pageCont(){
     this.textPagesService.addProject(this.lang).subscribe(data=>{
       this.pageData = data;
       console.log(this.pageData);
       this.title = this.pageData.addproject.title;
-      this.description = this.pageData.addproject.description;
+      this.description = this.sanitizer.bypassSecurityTrustHtml(this.pageData.addproject.description);
     });
   }
 
